38b1cbe0566ec8191c0aaf6b0d9eff7c21f5ca72
Kevin_Dhaduk <95095533+DhadukKevin@users.noreply.github.com>
2021-12-20 18:19:24 +0530

    Add files via upload

---
+++ prime_break.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+    void main()
+        {
+            int i,n,flag;
+            i=2;
+            flag=0;
+           
+
+            printf("Enter N:");
+            scanf("%d",&n);
+
+            while(i<n)
+            {
+                if(n%i==0)
+                {
+                   flag=1;
+                    break;
+                   
+                }
+                i++;
+              
+            }
+            if(flag==0)
+            {
+             printf("%d is prime",n);
+            }
+            else
+            {
+            printf("%d is not prime",n);
+            }
+            
+        }
